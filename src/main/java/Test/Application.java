f3a421523435cabee2bf739ddc0cdb4cb9e43dde
Ernst-Paul ten Brinke <eptb@schubergphilis.com>
2015-12-04 17:07:34 +0100

    add array with creates beans

--- src/main/java/Test/Application.java
+++ src/main/java/Test/Application.java
@@ -4,16 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.util.Arrays;
+
 /**
  * Created by etenbrinke on 04/12/15.
  */
-
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+
+
     }
 
 }
