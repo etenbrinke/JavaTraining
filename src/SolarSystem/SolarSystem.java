1302da8d60f332e995740a568af668d786879c7c
Ernst-Paul ten Brinke <eptb@schubergphilis.com>
2015-12-07 14:08:15 +0100

    Initial commit

---
+++ src/SolarSystem/SolarSystem.java
@@ -0,0 +1,11 @@
+package SolarSystem;
+
+/**
+ * Created by etenbrinke on 02/12/15.
+ */
+public class SolarSystem {
+
+    public SolarSystem() {
+    }
+
+}
---
+++ src/SomeTesting/Eptest3.java
@@ -0,0 +1,21 @@
+package JustMoreTraining;
+
+/**
+ * Created by etenbrinke on 06/11/15.
+ */
+public class Eptest3 {
+    public static void main(String[] args) {
+
+        String text="dfdf";
+        String black="dfdf";
+        String name="fd";
+
+        int test=2;
+
+        String greeting = text + black + name;
+
+        System.out.println(greeting+"df");
+        System.out.println(greeting+test);
+
+    }
+}
